6de86643e204c7262efe77963235300bae638916
Henry Arlt <arlthenry54@gmail.com>
2022-09-06 13:00:07 -0500

    fix GA

--- client/src/App.js
+++ client/src/App.js
@@ -6,18 +6,8 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import ContactMe from './components/ContactMe';
 import About from './components/About';
-import ReactGA from 'react-ga'
-
-const TRACKING_ID = "UA-238865861-1"
-ReactGA.initialize(TRACKING_ID)
-
-
 
 function App() {
-
-  useEffect(() => {
-    ReactGA.pageview(window.location.pathname);
-  })
   return (
     <div className="App">
       <div id='top'></div>
