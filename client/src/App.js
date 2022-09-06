c0c5e0b1e19bde7ec0089746e1b2ef3dad63d146
Henry Arlt <arlthenry54@gmail.com>
2022-09-06 12:35:48 -0500

    added GA

--- client/src/App.js
+++ client/src/App.js
@@ -6,6 +6,10 @@ import Projects from './components/Projects';
 import Skills from './components/Skills';
 import ContactMe from './components/ContactMe';
 import About from './components/About';
+import ReactGA from 'react-ga'
+require('dotenv').config()
+
+ReactGA.initialize(process.env.TRACKING_ID)
 
 
 function App() {
